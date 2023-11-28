4c32bab803dbbea3b46974682baf2123e54b619e
Shubham Koli <shubhamkk922@gmail.com>
2023-11-28 12:31:32 +0530

    commiting

--- controllers/product.js
+++ controllers/product.js
@@ -31,7 +31,7 @@ const getAllProducts = async (req,res)=>{
     let page = Number(req.query.page) || 1;
     let limit = Number(req.query.limit)|| 3;
     let skip = (page - 1) * limit;
-
+ 
     apiData = apiData.skip(skip).limit(limit);
 
     const myData = await apiData;
